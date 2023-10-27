e29fca0a9f8dfddaf74bae2357c50b31552ab8de
Spartan Leader <yegor.bushnev@gmail.com>
2023-10-27 00:07:28 -0400

    This is the final version of the encode program (Yegor)

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 """
-
-
+Lab 5
+Authors: Yegor Bushnev and Dustin Nguyen
+Class: COP3502C
+Section: 22282
+Description: Password encoder and decoder program that allows a user to go between encoding and
+decoding a password.
 """
+
 def encode(password):
+    """This function takes in a 8-digit password in string format containing only integers. The
+    function first converts every digit in the string into an integer and then increments each digit
+    by 3. The final string is the encoded password."""
     password_list = []
     encoded_list = []
     for i in password:
@@ -17,7 +25,9 @@ def encode(password):
     return encode_string
 
 def main():
+    """This is the function for the main program."""
     Program = True
+    #Create a while loop for the menu.
     while Program == True:
         print("""Menu 
     -------------
@@ -25,17 +35,22 @@ def main():
     2. Decode
     3. Quit""")
         print()
+        #Allow user to input a menu choice
         menu_choice = int(input("Please enter an option: "))
 
+        #Ecodes the password that the user inputs using the encode function.
         if menu_choice == 1:
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!")
             print()
 
+        #Decodes the encoded password of the user using the decode function
+        #Returns the original password
         elif menu_choice == 2:
             pass
 
+        #Quits the program
         elif menu_choice == 3:
             break
 
